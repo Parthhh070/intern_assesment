224391eb153eae75cd125c369842b8d2b2a53e34
Parthhh070 <ps381537@gmail.com>
2024-03-17 12:19:11 +0530

    intial commit

--- server/model/data.js
+++ server/model/data.js
@@ -1,30 +1,25 @@
 const mongoose = require("mongoose");
 
-// const DataSchema = new mongoose.Schema({
-//   end_year: String,
-//   intensity: Number,
-//   sector: String,
-//   topic: String,
-//   insight: String,
-//   url: String,
-//   region: String,
-//   start_year: String,
-//   impact: String,
-//   added: String,
-//   published: String,
-//   country: String,
-//   relevance: Number,
-//   pestle: String,
-//   source: String,
-//   title: String,
-//   likelihood: Number,
-// });
-
-const UserSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  age: Number,
+const DataSchema = new mongoose.Schema({
+  end_year: String,
+  intensity: Number,
+  sector: String,
+  topic: String,
+  insight: String,
+  url: String,
+  region: String,
+  start_year: String,
+  impact: String,
+  added: String,
+  published: String,
+  country: String,
+  relevance: Number,
+  pestle: String,
+  source: String,
+  title: String,
+  likelihood: Number,
 });
-const  userModel = mongoose.model("users", UserSchema);
 
-module.exports = userModel;
+const DataModel = mongoose.model("users", DataSchema);
+
+module.exports = DataModel;
